4e6fc800e607c51dddaa05bd7e47b391c6ed7993
Zayaan <72232563+grizzledwarvet23@users.noreply.github.com>
2023-02-18 12:53:17 -0600

    add term

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -9,6 +9,13 @@ def home():
 @app.route('/create-vocablist')
 def create_vocablist():
     return render_template('create.html')
+
+@app.route('/create', methods=['POST'])
+def process_submittedlist():
+    data = request.get_json()
+    print(data)
+    return jsonify({'message': 'Data received successfully'})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -10,3 +10,56 @@ function changeText() {
 function goToVocabListWindow() {
     window.location.href = "/create-vocablist";
 }
+
+function addTerm() {
+    var container = document.getElementById("terms-container");
+    var count = container.childElementCount + 1;
+  
+    var termGroup = document.createElement("div");
+    termGroup.className = "term-group";
+    var label = document.createElement("label");
+    label.setAttribute("for", "term" + count);
+    label.className = "form-label";
+    label.innerHTML = "Term " + count + ":";
+    var input = document.createElement("input");
+    input.setAttribute("type", "text");
+    input.setAttribute("name", "term" + count);
+    input.setAttribute("id", "term" + count);
+    input.setAttribute("placeholder", "Enter term...");
+    input.className = "form-input";
+    input.required = true;
+  
+    termGroup.appendChild(label);
+    termGroup.appendChild(input);
+  
+    container.appendChild(termGroup);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const form = event.target;
+    const formData = new FormData(form);
+    const formDataJSON = JSON.stringify(Object.fromEntries(formData));
+
+
+    fetch('/create', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: formDataJSON
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          // Redirect to home.html
+          window.location.href = '/create';
+        })
+        .catch(error => console.error(error));
+
+
+    //window.location.href = "/submittedlist";
+    
+    
+  }

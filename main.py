11b407bd581c31ae351af5b00991858db5c6289c
Zayaan <72232563+grizzledwarvet23@users.noreply.github.com>
2023-02-18 11:00:20 -0600

    create page

--- main.py
+++ main.py
@@ -4,12 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
 @app.route('/create-vocablist')
 def create_vocablist():
-    print('Button clicked')
-    return 'Button clicked'
+    return render_template('create.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
